1c79cd4bf680c8fba9a3e4e783c470009334120e
Oleg Plyska <op@nxc.no>
2016-08-19 11:37:00 +0300

    changes

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,7 +7,8 @@ import java.util.regex.Pattern;
 public class Calculator {
     private static final String ERROR_MESSAGE_IF_NUMBER_IS_WRONG = "Invalid number. Please, Try again";
     private static final Pattern PATTERN_FOR_OPERATORS = Pattern.compile("((?=.*[-+*/]).{0,1})");
-    private static final Pattern PATTERN_FOR_NUMBERS = Pattern.compile("((?=.*[.]).{0,1})(?=.*[0-9])");
+    //private static final Pattern PATTERN_FOR_NUMBERS = Pattern.compile("((?=.*[.]).{0,1})(?=.*[0-9])");
+    private static final Pattern PATTERN_FOR_NUMBERS = Pattern.compile("/d");
 
     private static String inputFirstNumber, inputSecondNumber,inputThirdNumber, inputOperator;
     // private static String firstOperator, secondOperator;
@@ -15,33 +16,25 @@ public class Calculator {
     private static Scanner scanner = new Scanner(System.in);
     private static String continueMessage;
 
-//    public static void setFirstNumber() {
-//
-//        System.out.println("First");
-//        inputFirstNumber = scanner.next();
-//        try {
-//            firstNumber = Double.parseDouble(inputFirstNumber);
-//        } catch (Exception e) {
-//            System.out.println(ERROR_MESSAGE_IF_NUMBER_IS_WRONG);
-//            setFirstNumber();
-//        }
-//    }
-
     public static void setFirstNumber() {
-
         System.out.println("First");
+        inputFirstNumber = scanner.next();
+        try {
+            firstNumber = Double.parseDouble(inputFirstNumber);
+        } catch (Exception e) {
+            System.out.println(ERROR_MESSAGE_IF_NUMBER_IS_WRONG);
+            setFirstNumber();
+        }
+    }
+
+ /*   public static void setFirstNumber() {
+        System.out.println("Write first number");
         inputFirstNumber = scanner.next();
         if (!PATTERN_FOR_NUMBERS.matcher(inputFirstNumber).matches()) {
             System.out.println("Invalid value. If value is double, then use point ( e.g. : 2.4 )");
             setFirstNumber();
         }
-    }
-
-
-
-
-
-
+    }*/
 
     public static void setOperator() {
         System.out.println("Write operator. Allowed operators: +-/*");
@@ -62,6 +55,15 @@ public class Calculator {
             setSecondNumber();
         }
     }
+
+/*    public static void setSecondNumber() {
+        System.out.println("Write second number");
+        inputSecondNumber = scanner.next();
+       if (!PATTERN_FOR_NUMBERS.matcher(inputSecondNumber).matches()) {
+           System.out.println("Invalid value. If value is double, then use point ( e.g. : 2.4 )");
+           setSecondNumber();
+       }
+    }*/
 
     public static void calculation() {
         switch (inputOperator) {
@@ -123,10 +125,19 @@ public class Calculator {
             thirdNumber = Double.parseDouble(inputThirdNumber);
         } catch (Exception e) {
             System.out.println(ERROR_MESSAGE_IF_NUMBER_IS_WRONG);
-            setSecondNumber();
+            setThirdNumber();
         }
-
     }
+
+/*public static void setThirdNumber() {
+    System.out.println("Write third number");
+    inputThirdNumber = scanner.next();
+    if (!PATTERN_FOR_NUMBERS.matcher(inputThirdNumber).matches()) {
+        System.out.println("Invalid value. If value is double, then use point ( e.g. : 2.4 )");
+        setThirdNumber();
+    }
+}*/
+
     private  static void calculationForThirdNumber() {
 
         switch (inputOperator) {
